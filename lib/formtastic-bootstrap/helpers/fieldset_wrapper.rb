b97305b7e51cd4eecde978caa7c7e8089c4d0744
AA-Ron <aaron.henderson@rfa.sc.gov>
2020-10-22 15:03:16 -0600

    QWEB-5014 refactor calls to Formtastic::Util

--- lib/formtastic-bootstrap/helpers/fieldset_wrapper.rb
+++ lib/formtastic-bootstrap/helpers/fieldset_wrapper.rb
@@ -1,21 +1,20 @@
 module FormtasticBootstrap
   module Helpers
     module FieldsetWrapper
-
       include Formtastic::Helpers::FieldsetWrapper
 
       protected
 
       def field_set_and_list_wrapping(*args, &block) #:nodoc:
-        contents = args.last.is_a?(::Hash) ? '' : args.pop.flatten
+        contents = args.last.is_a?(::Hash) ? "" : args.pop.flatten
         html_options = args.extract_options!
 
         if block_given?
           contents = if template.respond_to?(:is_haml?) && template.is_haml?
-            template.capture_haml(&block)
-          else
-            template.capture(&block)
-          end
+              template.capture_haml(&block)
+            else
+              template.capture(&block)
+            end
         end
 
         # Ruby 1.9: String#to_s behavior changed, need to make an explicit join.
@@ -23,13 +22,11 @@ module FormtasticBootstrap
 
         legend = field_set_legend(html_options)
         fieldset = template.content_tag(:fieldset,
-          Formtastic::Util.html_safe(legend) << Formtastic::Util.html_safe(contents),
-          html_options.except(:builder, :parent, :name)
-        )
+                                        legend.html_safe << contents.html_safe,
+                                        html_options.except(:builder, :parent, :name))
 
         fieldset
       end
-
     end
   end
 end
